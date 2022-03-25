7bea6a226cbb89777251e5c8e0a52d9f943a6f5a
William Pratt <willpratt78@gmail.com>
2022-03-25 10:43:40 -0700

    Fix HTML spacing, form tag placement, create variables in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+const data = parseInt($("#data")).val();
+const animal = parseInt($("#animal")).val();
+const color = parseInt($("#color")).val();
+const orginized = parseInt($("#orginized")).val();
+const active = parseInt($("#active")).val();
+
+console.log(data);
+console.log(animal);
+
+$(document).ready(function() {
+  
+})
