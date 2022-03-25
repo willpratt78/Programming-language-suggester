090e92f63e305c603840ac55419bfdbb3f396de2
William Pratt <willpratt78@gmail.com>
2022-03-25 10:53:09 -0700

    BUGFIX typo in variables so that .val works properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-const data = parseInt($("#data")).val();
-const animal = parseInt($("#animal")).val();
-const color = parseInt($("#color")).val();
-const orginized = parseInt($("#orginized")).val();
-const active = parseInt($("#active")).val();
+$(document).ready(function() {
+$("#quiz-form").submit(function(e) {
+  e.preventDefault();
+  const data = parseInt($("#data").val());
+  const animal = parseInt($("#animal").val());
+  const color = parseInt($("#color").val());
+  const orginized = parseInt($("#orginized").val());
+  const active = parseInt($("#active").val());
 
 console.log(data);
 console.log(animal);
-
-$(document).ready(function() {
-  
-})
+  });
+});
